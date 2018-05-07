6ac1235ecc68e745fd2f3f895b04734374d136f9
william <will@ivrforbeginners.com>
2018-05-06 20:12:13 -0400

    initial release

---
+++ app.js
@@ -0,0 +1,151 @@
+
+'use strict';
+
+// http://vanhack.ca/wp/events-calendar/
+
+//process.env.DEBUG = 'actions-on-google:*';
+var https = require('https');
+var httpStatus = require('http-status-codes');
+var ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
+var express = require('express');
+var expressApp = express();
+var bodyParser = require('body-parser');
+var Callback = require('./callback');
+var port = process.env.PORT || 8080;
+var VHS_EVENTS_QUERY = 'Events Query';
+
+function handlePost(request, response)
+{
+ var app, actions, parms;
+
+ // Javascript assistant API
+
+ parms = {};
+ parms.request = request;
+ parms.response = response;
+
+ app = new ActionsSdkApp(parms);
+
+ // Our intent to handler mapping
+
+ actions = new Map();
+ actions.set(VHS_EVENTS_QUERY,         queryIntent);
+ actions.set(app.StandardIntents.MAIN, mainIntent);
+ actions.set(app.StandardIntents.TEXT, textIntent);
+ app.handleRequest(actions);
+
+ // Handles the main intent - door is open?
+
+ function mainIntent(app)
+ {
+  var cb;
+
+  console.log('In main intent');
+
+  cb = new Callback();
+  cb.app = app;
+  getFromVHS(cb);
+ }
+
+ // Handles the query intent - next event is when?
+
+ function queryIntent(app)
+ {
+  var card, response;
+
+  console.log('In query intent');
+
+  card = app.buildBasicCard();
+  card.setTitle('Vancouver Hack Space')
+      .setSubtitle('Upcoming Events')
+      .addButton('View', 'http://vanhack.ca/wp/events-calendar/');
+
+  response = app.buildRichResponse();
+  response.addBasicCard(card)
+          .addSimpleResponse('Here you go!');
+
+  app.tell(response);
+ }
+
+ // Handles the text intent
+
+ function textIntent(app)
+ {
+  var text;
+
+  text = app.getRawInput() || '';
+  app.ask(text);
+ }
+}
+
+// Quick and dirty browser-based server health checker
+
+function handleGet(request, response)
+{
+ var date;
+
+ date = new Date();
+ 
+ response.send('Vancouver Hack Space says hello on ' +
+                 date.toLocaleDateString() +
+                   ' at ' +
+                     date.toLocaleTimeString());
+}
+
+// Sends a request to get the state of the door at Vancouver Hack Space
+
+function getFromVHS(cb)
+{
+ cb.text = '';
+ https.get('https://api.vanhack.ca/s/vhs/data/door.txt',
+         cb.to(getFromVHSCompletes));
+}
+
+// Runs when the request to VHS has been sent
+
+function getFromVHSCompletes(response, cb)
+{
+ response.on('data', function(chunk){ cb.text += chunk; });
+ response.on('end',  function(){ gotVHSResponse(cb, response.statusCode); });
+}
+
+// Runs when the response from VHS has arrived in full
+
+function gotVHSResponse(cb, code)
+{
+ var text;
+
+ text = cb.text;
+
+ if ( code == httpStatus.OK )
+  cb.app.tell('Vancouver Hack Space is ' + text + '.');
+
+ else
+  cb.app.tell('Sorry, I don\'t know whether the Vancouver Hack Space is open or not.');
+
+ cb.fulfill(text);
+ cb = null;
+}
+
+// Start the web server
+
+expressApp.set('port', port);
+expressApp.use( bodyParser.json( {type: 'application/json'} ) );
+
+// Handles POSTs from the Google Assistant
+
+expressApp.post('/', handlePost);
+
+// Handles GETs from a browser for checking for signs of life
+
+expressApp.get('/', handleGet);
+
+// Listen for connections
+
+expressApp.listen(port);
+
+// Show that we are ready to go
+
+console.log('Vancouver Hack Space Assistant listening on port %s', port);
+
+module.exports = expressApp;
---
+++ callback.js
@@ -0,0 +1,85 @@
+/**********************************************************************************
+MIT License
+
+Copyright (c) 2017 william depalo (will@ivrforbeginners.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**********************************************************************************/
+
+'use strict';
+
+var Promise = require('bluebird');
+
+function Callback()
+{
+ var _fn, _self, _resolve, _reject, _promise;
+
+ _fn = _resolve = _reject = null;
+ _self = this;
+
+ this.handler = function()
+ {
+  var i, parms;
+
+  for ( i = 0,  parms = []; i < arguments.length; i++ )
+   parms.push( arguments[i] );
+
+  parms.push(_self);
+
+  _fn.apply(this, parms);
+ };
+
+ this.to = function(callbackTo)
+ {
+  _fn = callbackTo;
+
+  return _self.handler;
+ };
+
+ this.newPromise = function()
+ {
+  return _promise = new Promise( function(res, rej){ _resolve = res; _reject = rej; } ).then(_no_op);
+ };
+
+ this.getPromise = function()
+ {
+  return _promise;
+ };
+
+ this.fulfill = function(value)
+ {
+  if ( _promise && _resolve )
+   _resolve.call(_promise, value);
+
+  _promise = null;
+ };
+
+ this.reject = function(err)
+ {
+  if ( _promise && _reject )
+   _reject.call(_promise, err);
+
+  _promise = null;
+ };
+}
+
+function _no_op()
+{
+}
+
+module.exports = Callback;
